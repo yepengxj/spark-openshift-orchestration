7cbad6c9182d5741b7df385e73925a799af97852
HeHongling <hank.sunday@gmail.com>
2016-04-08 16:02:28 +0800

    修改基础镜像来源，修改环境变量SPARK_MASTER

--- image/zepplin/zeppelin-env.sh
+++ image/zepplin/zeppelin-env.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-export MASTER="spark://spark-master:7077"
+export MASTER="spark://${SPARK_MASTER}:7077"
 export SPARK_HOME=/opt/spark
 export ZEPPELIN_JAVA_OPTS="-Dspark.jars=/opt/spark/lib/gcs-connector-latest-hadoop2.jar"
 # TODO(zmerlynn): Setting global CLASSPATH *should* be unnecessary,
