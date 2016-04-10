4557aa243ee015ad789af2d7526ff5581d73be40
HeHongling <hank.sunday@gmail.com>
2016-04-10 08:29:04 +0800

    修改spark home

--- image/zepplin/zeppelin-env.sh
+++ image/zepplin/zeppelin-env.sh
@@ -15,11 +15,7 @@
 # limitations under the License.
 
 export MASTER="spark://${SPARK_MASTER}:7077"
-export SPARK_HOME=/opt/spark
-export ZEPPELIN_JAVA_OPTS="-Dspark.jars=/opt/spark/lib/gcs-connector-latest-hadoop2.jar"
-# TODO(zmerlynn): Setting global CLASSPATH *should* be unnecessary,
-# but ZEPPELIN_JAVA_OPTS isn't enough here. :(
-export CLASSPATH="/opt/spark/lib/gcs-connector-latest-hadoop2.jar"
+export SPARK_HOME=/usr/share/spark
 export ZEPPELIN_NOTEBOOK_DIR="${ZEPPELIN_HOME}/notebook"
 export ZEPPELIN_MEM=-Xmx1024m
 export ZEPPELIN_PORT=8080
