53a409d4e785c6bc23d307e6d5e144096e24f747
yepengxj <yepeng_xj@163.com>
2016-05-09 21:17:15 +0800

    Update docker-zeppelin.sh

--- image/zepplin/docker-zeppelin.sh
+++ image/zepplin/docker-zeppelin.sh
@@ -23,6 +23,9 @@ eval SPARK_SECRET=\$BSI_${SPARKBSI}_PASSWORD
 echo $SPARK_MASTER
 echo $SPARK_SECRET
 
+export $SPARK_MASTER
+export $SPARK_SECRET
+
 if [[ -n "${SPARK_MASTER}" ]]; then
   sed -i "s/SPARK_MASTER/${SPARK_MASTER}/" /opt/spark/conf/spark-defaults.conf
 fi
